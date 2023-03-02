bb9e4ffcc9ce527ee21e1e89f7489658585e56e7
Anton Soares <asoares@butterflynetinc.com>
2023-03-02 10:36:26 -0500

    [FUT-77] Setup dependencies

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -7,10 +7,30 @@ android {
 }
 
 dependencies {
-    // Remove this dummy dependencies and add your own.
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // KTX
+    implementation(Libs.coreKtx)
+    implementation(Libs.lifecycleKtx)
+
+    // Retrofit setup
+    implementation(Libs.retrofit)
+    implementation(Libs.retrofitJacksonConverter)
+    implementation(Libs.okHttp)
+    implementation(Libs.okHttpLoggingInterceptor)
+
+    // Custom tabs
+    implementation(Libs.customTabs)
+
+    // Encrypted Shared Prefs and ID token resolution
+    implementation(Libs.androidSecurity)
+    implementation(Libs.googleApiClient) {
+        exclude("org.apache.httpcomponents")
+    }
+
+    implementation(Libs.androidAppCompat)
+    implementation(Libs.material)
+
+    // Test dependencies
+    testImplementation(Libs.junit)
+    androidTestImplementation(Libs.androidJunit)
+    androidTestImplementation(Libs.esspreso)
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,3 +4,33 @@ object BuildPlugins {
     val detekt by lazy { "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${Versions.detekt}" }
     val jacoco by lazy { "org.jacoco:org.jacoco.core:${Versions.jacoco}" }
 }
+
+object Libs {
+    // KTX
+    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val lifecycleKtx by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleKtx}" }
+
+    //Retrofit
+    val retrofit by lazy { "com.squareup.retrofit2:retrofit:${Versions.retrofit}" }
+    val retrofitJacksonConverter by lazy { "com.squareup.retrofit2:converter-jackson:${Versions.retrofit}" }
+    val okHttp by lazy { "com.squareup.okhttp3:okhttp:${Versions.okhttp}" }
+    val okHttpLoggingInterceptor by lazy { "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}" }
+
+    // Custom Tabs
+    val customTabs by lazy { "androidx.browser:browser:${Versions.customTabs}" }
+
+    // Security
+    val androidSecurity by lazy { "androidx.security:security-crypto:${Versions.androidSecurity}" }
+
+    // Google Api Client
+    val googleApiClient by lazy { "com.google.api-client:google-api-client:${Versions.googleApiClient}" }
+
+    // Android
+    val androidAppCompat by lazy { "androidx.appcompat:appcompat:${Versions.androidAppCompat}" }
+    val material by lazy { "com.google.android.material:material:${Versions.material}" }
+
+    // Testing
+    val junit by lazy { "junit:junit:${Versions.junit}" }
+    val androidJunit by lazy { "androidx.test.ext:junit:${Versions.androidJunit}" }
+    val esspreso by lazy { "androidx.test.espresso:espresso-core:${Versions.espresso}" }
+}
