2e34dd120674d880eddff548012af047322bfa18
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-02-14 11:27:08 +0100

    delete generated docs
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- docs/generated/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":auth-api:dokkaHtml/androidTestRelease":[],":auth-api:dokkaHtml/debug":[],":auth-api:dokkaHtml/main":[],":auth-api:dokkaHtml/release":[],":auth-api:dokkaHtml/testFixtures":[],":auth-api:dokkaHtml/testFixturesDebug":[],":auth-api:dokkaHtml/testFixturesRelease":[]}'
