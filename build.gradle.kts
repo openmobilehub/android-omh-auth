ccd0bba86b50c7696a0287ce4324e33a3c066121
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-03-13 21:33:58 +0100

    Prevent publishing to Sonatype when useMavenLocal
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,23 +90,25 @@ tasks {
     getByName("prepareKotlinBuildScriptModel").dependsOn(installPreCommitHook)
 }
 
-val ossrhUsername by extra(getValueFromEnvOrProperties("OSSRH_USERNAME"))
-val ossrhPassword by extra(getValueFromEnvOrProperties("OSSRH_PASSWORD"))
-val mStagingProfileId by extra(getValueFromEnvOrProperties("SONATYPE_STAGING_PROFILE_ID"))
-val signingKeyId by extra(getValueFromEnvOrProperties("SIGNING_KEY_ID"))
-val signingPassword by extra(getValueFromEnvOrProperties("SIGNING_PASSWORD"))
-val signingKey by extra(getValueFromEnvOrProperties("SIGNING_KEY"))
+if (!useMavenLocal) {
+    val ossrhUsername by extra(getValueFromEnvOrProperties("OSSRH_USERNAME"))
+    val ossrhPassword by extra(getValueFromEnvOrProperties("OSSRH_PASSWORD"))
+    val mStagingProfileId by extra(getValueFromEnvOrProperties("SONATYPE_STAGING_PROFILE_ID"))
+    val signingKeyId by extra(getValueFromEnvOrProperties("SIGNING_KEY_ID"))
+    val signingPassword by extra(getValueFromEnvOrProperties("SIGNING_PASSWORD"))
+    val signingKey by extra(getValueFromEnvOrProperties("SIGNING_KEY"))
 
-// Set up Sonatype repository
-nexusPublishing {
-    repositories {
-        sonatype {
-            stagingProfileId.set(mStagingProfileId.toString())
-            username.set(ossrhUsername.toString())
-            password.set(ossrhPassword.toString())
-            // Add these lines if using new Sonatype infra
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+    // Set up Sonatype repository
+    nexusPublishing {
+        repositories {
+            sonatype {
+                stagingProfileId.set(mStagingProfileId.toString())
+                username.set(ossrhUsername.toString())
+                password.set(ossrhPassword.toString())
+                // Add these lines if using new Sonatype infra
+                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+                snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            }
         }
     }
 }
