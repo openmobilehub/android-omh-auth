0984d7d9004b7358ff09c12988115f3a70fa8999
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-03-14 14:44:33 +0100

    Optimize secrets loading
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

---
+++ .github/scripts/setup_secrets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p ~/.gradle
+echo "GOOGLE_CLIENT_ID=$1" >> ~/.gradle/gradle.properties
+echo "FACEBOOK_APP_ID=$2" >> ./local.properties
+echo "FACEBOOK_CLIENT_TOKEN=$3" >> ./local.properties
+echo "MICROSOFT_CLIENT_ID=$4" >> ./local.properties
+echo "MICROSOFT_SIGNATURE_HASH=$5" >> ./local.properties
+echo "DROPBOX_APP_KEY=$6" >> ./local.properties
