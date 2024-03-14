8b909c47ea1a1b0d996d4d8752017dfd160e84f3
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-03-14 16:26:47 +0100

    Fix Google Client ID secret loading
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- .github/scripts/setup_secrets.sh
+++ .github/scripts/setup_secrets.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mkdir -p ~/.gradle
-echo "GOOGLE_CLIENT_ID=$1" >> ~/.gradle/gradle.properties
+echo "GOOGLE_CLIENT_ID=$1" >> ./local.properties
 echo "FACEBOOK_APP_ID=$2" >> ./local.properties
 echo "FACEBOOK_CLIENT_TOKEN=$3" >> ./local.properties
 echo "MICROSOFT_CLIENT_ID=$4" >> ./local.properties
