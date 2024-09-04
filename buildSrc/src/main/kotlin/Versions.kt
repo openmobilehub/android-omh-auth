e3cecfffc694385ef12dd5d1d565ed4c2415daac
Damian Nowak <damian.nowak.5f@gmail.com>
2024-09-04 13:14:55 +0200

    release: 2.0.4 (#105)
    Signed-off-by: DamianNowak5f <damian.nowak.5f@gmail.com>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -45,12 +45,12 @@ object Versions {
 
     // OMH
     const val omhCore = "2.0.1-beta"
-    const val omhAuthCore = "2.0.3"
-    const val omhGoogleGms = "2.0.3"
-    const val omhGoogleNonGms = "2.0.3"
-    const val omhFacebook = "2.0.3"
-    const val omhMicrosoft = "2.0.3"
-    const val omhDropbox = "2.0.3"
+    const val omhAuthCore = "2.0.4"
+    const val omhGoogleGms = "2.0.4"
+    const val omhGoogleNonGms = "2.0.4"
+    const val omhFacebook = "2.0.4"
+    const val omhMicrosoft = "2.0.4"
+    const val omhDropbox = "2.0.4"
 
     // Google Sign In
     const val googleSignIn = "20.4.1"
