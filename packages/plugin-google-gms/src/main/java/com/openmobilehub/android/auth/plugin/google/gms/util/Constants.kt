fa453848e4426dc0207b5caac3cce584dc1bc063
Adam Trzciński <trzcinski.adam@gmail.com>
2024-10-22 12:51:09 +0200

    fix: use Constants for implementation paths (#108)
    Signed-off-by: Adam Trzciński <trzcinski.adam@gmail.com>

---
+++ packages/plugin-google-gms/src/main/java/com/openmobilehub/android/auth/plugin/google/gms/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.openmobilehub.android.auth.plugin.google.gms.util
+
+object Constants {
+    const val IMPLEMENTATION_PATH = "com.openmobilehub.android.auth.plugin.google.gms.OmhAuthFactoryImpl"
+}
